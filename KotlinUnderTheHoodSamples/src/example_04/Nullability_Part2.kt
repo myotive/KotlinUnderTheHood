8101a0c9882c5a1db045d089b68f325467e4b1ef
Michael Yotive <myotive@gmail.com>
2017-09-24 16:15:06 -0400

    Fixing a thing...

--- KotlinUnderTheHoodSamples/src/example_04/Nullability_Part2.kt
+++ KotlinUnderTheHoodSamples/src/example_04/Nullability_Part2.kt
@@ -3,18 +3,18 @@ package example_04
 /**
  * Nullable class A
  */
-/*class A{
+class A{
     var temp: String? = null
-}*/
+}
 
 /**
  * Not-null Class B
  */
-//class B{
-//    var temp2: String = ""
-//}
+/*class B{
+    var temp2: String = ""
+}*/
 
 /**
  * Not-null Class C with 3 non-nullable constructor parameters
  */
-class C (var a : String, var b: String, var c: String)
+/*class C (var a : String, var b: String, var c: String)*/
